f0379573535262c632bbff542e927ee7571e9556
Nicolas Pellegrin <npellegrin.dev@gmail.com>
2012-12-08 20:56:32 +0100

    Add a character, with correct z order

--- js/maze.js
+++ js/maze.js
@@ -4,29 +4,6 @@ var roadSize = {width:64, height:64};
 
 // Maze (get from server later)
 var mazeSize = {width:20, height:20};
-/*var mazePattern = Array(
-	[15,15,15,15,15,15,15,15,15,14,14,15,15,15,15,15,15,15,15,15],
-	[15,15,15,15,15,15,15,15,14,14,14,14,15,15,15,15,15,15,15,15],
-	[15,15,15,15,15,15,15,14,14,14,14,14,14,15,15,15,15,15,15,15],
-	[15,15,15,15,15,15,14,14,14,14,14,14,14,14,15,15,15,15,15,15],
-	[15,15,15,15,15,14,14,14,14,14,14,14,14,14,14,15,15,15,15,15],
-	[15,15,15,15,14,14,14,14,14,14,14,14,14,14,14,14,15,15,15,15],
-	[15,15,15,14,14,14,14,14,14,14,14,14,14,14,14,14,14,15,15,15],
-	[15,15,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,15,15],
-	[15,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,15],
-	[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14],
-	[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14],
-	[15,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,15],
-	[15,15,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,15,15],
-	[15,15,15,14,14,14,14,14,14,14,14,14,14,14,14,14,14,15,15,15],
-	[15,15,15,15,14,14,14,14,14,14,14,14,14,14,14,14,15,15,15,15],
-	[15,15,15,15,15,14,14,14,14,14,14,14,14,14,14,15,15,15,15,15],
-	[15,15,15,15,15,15,14,14,14,14,14,14,14,14,15,15,15,15,15,15],
-	[15,15,15,15,15,15,15,14,14,14,14,14,14,15,15,15,15,15,15,15],
-	[15,15,15,15,15,15,15,15,14,14,14,14,15,15,15,15,15,15,15,15],
-	[15,15,15,15,15,15,15,15,15,14,14,15,15,15,15,15,15,15,15,15]
-);
-*/
 var mazePattern = Array(
 	[15,15,15,15,15,15,15,15,15, 8,13,15,15,15,15,15,15,15,15,15],
 	[15,15,15,15,15,15,15,15, 0,10, 1, 1,15,15,15,15,15,15,15,15],
@@ -50,8 +27,16 @@ var mazePattern = Array(
 	[15,15,15,15,15,15,15,15,15, 7, 4,15,15,15,15,15,15,15,15,15]
 );
 
-// Images
-var imageFilenames = Array("northSouth.png", "westEast.png",		//  0  1
+// Character images
+var characterImageFilenames = Array("character_north.png", "character_south.png", "character_east.png", "character_west.png");
+var characterImages = Array();
+for(var i=0; i<characterImageFilenames.length; i++) {
+	characterImages[i] = new Image();
+	characterImages[i].src = "img/"+characterImageFilenames[i];
+}
+
+// Wall images
+var wallImageFilenames = Array("northSouth.png", "westEast.png",		//  0  1
 	"northSouthDeadEnd.png", "northSouthDeadStart.png",				//  2  3
 	"westEastDeadEnd.png", "westEastDeadStart.png",					//  4  5
 	"cornerSouthEast.png", "cornerNorthEast.png", 					//  6  7
@@ -59,11 +44,14 @@ var imageFilenames = Array("northSouth.png", "westEast.png",		//  0  1
 	"westEastIntersectSouth.png", "westEastIntersectNorth.png",		// 10 11
 	"northSouthIntersectWest.png", "northSouthIntersectEast.png",	// 12 13
 	"cross.png", "blank.png");										// 14 15
-var images = Array();
-for(var i=0; i<imageFilenames.length; i++) {
-	images[i] = new Image();
-	images[i].src = "img/"+imageFilenames[i];
+var wallImages = Array();
+for(var i=0; i<wallImageFilenames.length; i++) {
+	wallImages[i] = new Image();
+	wallImages[i].src = "img/"+wallImageFilenames[i];
 }
+
+// Game objects
+var character = {x:50, y:70, drawn:false};
 
 $(document).ready(function() {
 	// Set canvas size (depending road size and maze size)
@@ -71,21 +59,32 @@ $(document).ready(function() {
 	canvas.setAttribute("width", roadSize.width*(mazeSize.width/2));
 	canvas.setAttribute("height", roadSize.height*(mazeSize.height/2));
 	
-	// draw maze !
+	// Draw maze !
 	drawMaze();
 });
 
 function drawMaze () {
+	// Get environment
 	var canvas = document.getElementById("maze");
 	var ctx_canvas = canvas.getContext("2d");
 
 	var offsetX = -(mazeSize.width*roadSize.width-roadSize.width)/4-roadSize.width/4;
 	var offsetY = (mazeSize.height*roadSize.height-roadSize.height)/4-roadSize.height/4;
+	character.drawn = false;
 	for(var y=0; y<mazeSize.height; y++) {
 		for(var x=0; x<mazeSize.width; x++) {
-			ctx_canvas.drawImage(images[mazePattern[y][x]],
-			(x+y)*roadSize.width/2+offsetX,
-			(y-x)*roadSize.height/2+offsetY);
+			// Compute coords
+			var wallX = (x+y)*roadSize.width/2+offsetX;
+			var wallY = (y-x)*roadSize.height/2+offsetY;
+			
+			// Draw character (depending depth)
+			if(!character.drawn && wallX<character.y) {
+				ctx_canvas.drawImage(characterImages[2], character.x, character.y);
+				character.drawn = true;
+			}
+
+			// Draw walls
+			ctx_canvas.drawImage(wallImages[mazePattern[y][x]], wallX, wallY);
 		}
 	}
 }
